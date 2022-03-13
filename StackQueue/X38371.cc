23cff8a5c27d3bb585e3385bb6ee842bcb1c29ab
pablofc18 <pablofc18@gmail.com>
2022-03-13 20:47:18 +0100

    2/3 Jtg

--- StackQueue/X38371.cc
+++ StackQueue/X38371.cc
@@ -9,14 +9,14 @@ void calc_min_max_med(queue<int> c, int &min, int &max, double &media)
   while (not c.empty()) {
     media += c.front();
     if (c.front() < min) min = c.front();
-    if (c.front() > max) max = c.front();
+    else if (c.front() > max) max = c.front();
     c.pop();
   }
   media /= num_tot;
 }
 
 
-void calcular(const queue<int> &c, int &min, int &max, double media)
+void calcular(const queue<int> &c, int min, int max, double media)
 /*Pre: no vacía*/
 /*Post: dice el min el max y la media de los numeros que hay en la queue*/
 {
@@ -30,9 +30,9 @@ void calcular(const queue<int> &c, int &min, int &max, double media)
 
 int main() {
   queue<int> c;
-  int num; cin >> num;
   int min = 0, max = 0;
   double media = 0;
+  int num; cin >> num;
   while (num <= 1000 and num >= -1001) {
     if (not c.empty() and num == -1001) {
       c.pop();
