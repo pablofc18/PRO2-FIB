4404ccc6b93c5a80153d4d426b74e0e256eac02c
pablofc18 <pablofc18@gmail.com>
2022-03-23 21:13:04 +0100

    7/8 jutge

---
+++ parcial1_teoria/X78332/main.cc
@@ -0,0 +1,40 @@
+#include "BinTree.hh"
+#include <stack>
+// #include "BinTreeIOint.hh"
+
+
+int mida(const BinTree<int> &a)
+{
+  if (a.empty()) return 0;
+  else return 1 + mida(a.left()) + mida(a.right());
+}
+
+void cami_preferent(const BinTree<int>& a, stack<int>& c)
+ /* Pre: c esta vacia */
+ /* Post: c contiene el camino preferente de a; si no esta vacia, el primer
+          elemento del camino esta en la cima de c */
+{
+  if (not a.empty()) {
+    if (mida(a.left())<mida(a.right())) cami_preferent(a.right(), c);
+    else cami_preferent(a.left(), c);
+    c.push(a.value());
+  }
+}
+
+// void write_stack(stack<int> p)
+// {
+//   while(not p.empty()) {
+//     cout << p.top();
+//     p.pop();
+//   }
+// }
+
+// int main() {
+//   BinTree<int> a;
+//   read_bintree_int(a, 0);
+//   stack<int> p;
+//
+//   cami_preferent(a, p);
+//   write_stack(p);
+//
+// }
