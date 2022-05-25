86ab89534cd252d42f4dafbb923a8ab9ff0691e3
pablofc18 <pablofc18@gmail.com>
2022-05-25 13:22:49 +0200

    ex arbre ptr

---
+++ Apuntadors1/X15014/main.cc
@@ -0,0 +1,7 @@
+#include "ArbIOint.hh"
+
+int main() {
+    Arbre<int> a;
+    llegir_arbre_int(a, 0);
+    escriure_arbre_int(a);
+}
--- Apuntadors1/X15014/program.hh
+++ Apuntadors1/X15014/program.hh
@@ -1,6 +1,23 @@
 void arb_sumes(Arbre<int> &asum) const
 /* Pre: cert */
-/* Post: l'arbre asum és l'arbre suma del p.i. */
+/* Post: l'arbre asum Ã©s l'arbre suma del p.i. */
 {
-    asum.primer_node
+  asum.primer_node = i_arb_sumes(primer_node);
+}
+
+private:
+
+static node_arbre* i_arb_sumes(node_arbre* m)
+{
+  node_arbre* n;
+  if (m == nullptr) n = nullptr;
+  else {
+    n = new node_arbre;
+    n->info = m->info;
+    n->segE = i_arb_sumes(m->segE);
+    if (n->segE != nullptr) n->info += (n->segE)->info;
+    n->segD = i_arb_sumes(m->segD);
+    if (n->segD != nullptr) n->info += (n->segD)->info; 
+  }
+  return n;
 }
